5b6a97b8e3e0a27ff16d0eb86f1a0951b320ed3c
harunhira69 <harunabhi4@gmail.com>
2025-11-02 01:33:37 +0600

    added some functionality

--- src/Component/MyBids/MyBids.jsx
+++ src/Component/MyBids/MyBids.jsx
@@ -1,9 +1,118 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../Context/AuthContext';
+import Swal from 'sweetalert2';
 
 const MyBids = () => {
+    const {user} = useContext(AuthContext)
+    const [bids,setBids] = useState([])
+    useEffect(()=>{
+        if(user?.email){
+
+             fetch(`http://localhost:3000/bids?email=${user.email}`)
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+            setBids(data)
+        })
+    }
+        }
+       ,[user.email])
+
+
+
+
+       const handleDeleteBid =(_id)=>{
+        Swal.fire({
+  title: "Are you sure?",
+  text: "You won't be able to revert this!",
+  icon: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#3085d6",
+  cancelButtonColor: "#d33",
+  confirmButtonText: "Yes, delete it!"
+}).then((result) => {
+  if (result.isConfirmed) {
+    fetch(`http://localhost:3000/bids/${_id}`,{
+        method:"DELETE"
+    })
+    .then(res=>res.json())
+    .then(data=>{
+       
+        if(data.deletedCount>0){
+      Swal.fire({
+      title: "Deleted!",
+      text: "Your bid has been deleted.",
+      icon: "success"
+    });
+        }
+    })
+  
+  }
+  const remainingBid = bids.filter(bid=>bid._id!==_id)
+  setBids(remainingBid)
+});
+
+       }
     return (
         <div>
-            This is Bids pages
+           <h3>MY BID:{bids.length}</h3>
+           <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+      
+        <th>#</th>
+        <th>Product</th>
+        <th>Seller</th>
+        <th>Bid Price</th>
+        <th>Status</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+   {
+                bids.map((bid, index) => <tr>
+                  <td>
+                    {index + 1}
+                  </td>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                          <img
+                            src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                            alt="Avatar Tailwind CSS Component" />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">{bid.buyer_name}</div>
+                        <div className="text-sm opacity-50">United States</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    {bid.buyer_email}
+
+                  </td>
+                  <td>{bid.bid_price}</td>
+                  <td>{bid.status==='pending'?<div class="badge badge-warning">{bid.status}</div>:<div class="badge badge-success">{bid.status}</div>}</td>
+                  <th>
+                    <button 
+                    onClick={()=>handleDeleteBid(bid._id)}
+                    className="btn btn-outline btn-xs">
+                        Remove Bid</button>
+                  </th>
+                </tr>)
+              }
+   
+     
+    </tbody>
+
+
+  </table>
+</div>
         </div>
     );
 };
--- src/Component/ProductDetails/ProductDetails.jsx
+++ src/Component/ProductDetails/ProductDetails.jsx
@@ -1,14 +1,192 @@
-import React from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { useLoaderData } from 'react-router';
+import { AuthContext } from '../../Context/AuthContext';
+import Swal from 'sweetalert2';
 
 const ProductDetails = () => {
-    const product = useLoaderData();
-    console.log(product)
-    return (
-        <div>
-            <h3>This is fucking product details</h3>
+  const { _id: productId } = useLoaderData();
+  const { user } = useContext(AuthContext)
+  const [bids, setBids] = useState([])
+
+  const bidModalRef = useRef(null)
+
+
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/product/bids/${productId}`)
+      .then(res => res.json())
+      .then(data => {
+        // Ensure we always set an array
+
+        setBids(data)
+        console.log('Bids for this product:', data);
+      })
+      .catch(err => console.error('Error loading bids:', err));
+  }, [productId]);
+
+
+
+
+  const handleBidModal = () => {
+    bidModalRef.current.showModal()
+  }
+
+
+  const handleBidSubmit = e => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const bid = e.target.bid.value;
+    console.log({ productId, name, email, bid })
+
+    const newBid = {
+      product: productId,
+      buyer_name: name,
+      buyer_email: email,
+      bid_price: bid,
+
+    }
+    fetch('http://localhost:3000/bids', {
+      method: 'POST',
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(newBid)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log('after bid', data)
+        if(data.insertedId){
+          bidModalRef.current.close()
+            Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Your bid has been placed",
+          showConfirmButton: false,
+          timer: 1500
+        });
+        newBid._id=data.insertedId;
+        const newBids = [...bids,newBid]
+        newBids.sort((a,b)=>b.bid_price-a.bid_price)
+        setBids(newBids)
+        }
+      
+      })
+
+  }
+
+
+  return (
+    <div>
+      <div>
+
+      </div>
+      {/* product info */}
+      <div>
+        <button
+          onClick={handleBidModal}
+          className="btn btn-primary w-full">I want to Buy this product</button>
+
+        <dialog ref={bidModalRef} className="modal modal-bottom sm:modal-middle">
+          <div className="modal-box">
+            <form onSubmit={handleBidSubmit}>
+              <fieldset className="fieldset">
+                {/* name */}
+                <label className="label">Name</label>
+                <input type="text" className="input"
+                  readOnly
+                  name='name'
+                  defaultValue={user?.displayName} />
+                <label className="label">Email</label>
+
+                <input type="email"
+                  className="input" readOnly
+                  defaultValue={user?.email}
+                  name='email' />
+
+                {/* bid */}
+                <label className="label">Bid</label>
+                <input type="text" className="input" name='bid'
+                  placeholder='your bid amount'
+
+                />
+                <button className="btn btn-neutral mt-4">Place Your Bid</button>
+              </fieldset>
+
+            </form>
+            <div className="modal-action">
+              <form method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn">Close</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+      </div>
+      {/* bids for this product */}
+      <div>
+        <h3 className='text-3xl'>Bids for this product: <span className='text-purple-400'>{bids.length}</span></h3>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>
+                  SL No.
+                </th>
+                <th>Buyer Name</th>
+                <th>Buyer Email</th>
+                <th>Bid Price</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+             
+              {
+                bids.map((bid, index) => <tr>
+                  <th>
+                    {index + 1}
+                  </th>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                          <img
+                            src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                            alt="Avatar Tailwind CSS Component" />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">{bid.buyer_name}</div>
+                        <div className="text-sm opacity-50">United States</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    {bid.buyer_email}
+
+                  </td>
+                  <td>{bid.bid_price}</td>
+                  <th>
+                    <button className="btn btn-ghost btn-xs">details</button>
+                  </th>
+                </tr>)
+              }
+             
+
+
+
+            </tbody>
+            {/* foot */}
+
+
+
+          </table>
         </div>
-    );
+      </div>
+    </div>
+
+  );
 };
 
 export default ProductDetails;
